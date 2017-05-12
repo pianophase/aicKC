ee15bae2d2c3dc0f65280348f04afe76058cc704
pablinha <nagoyamarimbas@gmail.com>
2017-05-12 14:55:20 -0300

    Add files via upload
    Faster than the original

---
+++ AIC_S.R
@@ -0,0 +1,11 @@
+## Robust AIC (based on Claeskens et al.)
+AIC_S <- function(y, X, beta, scale, cc) {
+    re  <- drop((y - X %*% beta) / scale)
+    U2  <- Mchi(re, cc = cc, psi = 'bisquare', deriv = 2)
+    U1  <- Mchi(re, cc = cc, psi = 'bisquare', deriv = 1)
+    J   <- crossprod(X, U2 * X)
+    K   <- crossprod(U1 * X)
+    tr  <- sum(diag(solve(J, K)))
+    gof <- 2 * length(y) * log(scale)
+    return(list(AIC = gof + 2 * tr, traza = tr))
+}
